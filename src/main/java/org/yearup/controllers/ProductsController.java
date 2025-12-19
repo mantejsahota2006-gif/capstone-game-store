d2726b173c62a855ba4af36512aba6ec65603a49
mantej <https://github.com/mantejsahota@2006-gif/My-First-Repo.git>
2025-12-19 01:12:36 -0500

    product get by id

--- src/main/java/org/yearup/controllers/ProductsController.java
+++ src/main/java/org/yearup/controllers/ProductsController.java
@@ -43,16 +43,17 @@ public class ProductsController
     @PreAuthorize("permitAll()")
     public Product getById(@PathVariable int id)
     {
-        try {
-            Product product = productDao.getById(id);
-            if (product == null)
+        try
+        {
+            var product = productDao.getById(id);
+
+            if(product == null)
                 throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
             return product;
         }
-        catch (ResponseStatusException ex) {
-            throw ex;
-        }
-        catch (Exception ex) {
+        catch(Exception ex)
+        {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Oops... our bad.");
         }
     }
